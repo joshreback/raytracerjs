be5c6d4665db3b1938af31606a86d2daa8af15bf
Josh Reback <jreback@careersofia.com>
2017-09-02 23:50:19 -0700

    complete project one

--- raytracer.js
+++ raytracer.js
@@ -1,42 +1,33 @@
 class RayTracer {
-  constructor(camera, imagePlane) {
-    this.camera = camera;
-    this.imagePlane = imagePlane
+  constructor(scene, w, h) {
+    this.scene = scene;
+    this.w = w;
+    this.h = h;
+
   }
 
   scaledCoords(x, y){
     return {
-      x: (x + 1) * (WIDTH / 2),
-      y: (y + 0.75) * (HEIGHT / 1.5)
+      x: (x + 1) * (this.w / 2),
+      y: (y + 0.75) * (this.h / 1.5)
     }
   }
 
   colorAtCoordinate(x, y) {
     let alpha, beta, top, bottom, point, ray;
 
-    alpha = x / WIDTH;
-    beta = y / HEIGHT;
-    top = Vector.lerp(this.imagePlane.vec1, this.imagePlane.vec2, alpha);
-    bottom = Vector.lerp(this.imagePlane.vec3, this.imagePlane.vec4, alpha);
+    alpha = x / this.w;
+    beta = y / this.h;
+    top = Vector.lerp(this.scene.imagePlane.vec1, this.imagePlane.vec2, alpha);
+    bottom = Vector.lerp(this.scene.imagePlane.vec3, this.imagePlane.vec4, alpha);
     point = Vector.lerp(top, bottom, beta);
     ray = new Ray(
       point,
-      point.minus(camera)
+      point.minus(this.scene.camera)
     );
 
     let scaledCoords = this.scaledCoords(ray.direction.x, ray.direction.y);
     return new Color(scaledCoords.x, scaledCoords.y, 0);
-  }
-
-  trace() {
-    let color;
-    for (let y = 0; y < HEIGHT; y++) {
-      for (let x = 0; x < WIDTH; x++) {
-        color = this.colorAtCoordinate(x, y);
-
-        image.putPixel(x, y, color);
-      }
-    }
   }
 }
 const WIDTH = 256;
@@ -45,15 +36,26 @@ const HEIGHT = 192;
 const image = new Image(WIDTH, HEIGHT);
 document.image = image;
 
-const camera = new Vector(0, 0, -1);
-const imagePlane = new ImagePlane(
-  new Vector(1, 0.75, 0),
-  new Vector(-1, 0.75, 0),
-  new Vector(1, -0.75, 0),
-  new Vector(-1, -0.75, 0)
-)
+const SCENE = {
+  camera: new Vector(0, 0, -1),
+  imagePlane: new ImagePlane(
+    new Vector(1, 0.75, 0),
+    new Vector(-1, 0.75, 0),
+    new Vector(1, -0.75, 0),
+    new Vector(-1, -0.75, 0)
+  )
+}
 
-tracer = new RayTracer(camera, imagePlane);
-tracer.trace();
+const tracer = new RayTracer(SCENE, WIDTH, HEIGHT);
+
+for (let y = 0; y < HEIGHT; y++) {
+  for (let x = 0; x < WIDTH; x++) {
+    image.putPixel(
+      x,
+      y,
+      this.colorAtCoordinate(x, y);
+    );
+  }
+}
 
 image.renderInto(document.querySelector('body'));
